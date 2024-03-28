e40f03d5c9cfe735f24d232bb7cbc2b192bbe89e
Mohamad Himuni <sazyo.com@gmail.com>
2024-03-29 01:08:13 +0200

    test

--- src/test/java/AcceptanceTest/login_User.java
+++ src/test/java/AcceptanceTest/login_User.java
@@ -25,8 +25,7 @@ public class login_User {
 
   @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user enters valid username and password")
