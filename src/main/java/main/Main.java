65599aa01f3966d9308ac5169646814a531e5347
talaabusoud <159799893+talaabusoud@users.noreply.github.com>
2024-03-30 23:41:41 +0300

    Update Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -203,7 +203,6 @@ public class Main {
                     displayDownLine();
                     break;
             }
-//        }
     }
     //sign up page (register user)
     public static void signUpPage(){
